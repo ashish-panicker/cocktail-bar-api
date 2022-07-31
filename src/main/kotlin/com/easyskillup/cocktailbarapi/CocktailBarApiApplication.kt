ef7f15bad45991da4200bef6b42159cdc598a535
ashish-panicker <ashish.s.panicker@outlook.com>
2022-07-31 10:00:52 +0530

    Gettign started

---
+++ src/main/kotlin/com/easyskillup/cocktailbarapi/CocktailBarApiApplication.kt
@@ -0,0 +1,11 @@
+package com.easyskillup.cocktailbarapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CocktailBarApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CocktailBarApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/easyskillup/cocktailbarapi/CocktailBarApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.easyskillup.cocktailbarapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CocktailBarApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
